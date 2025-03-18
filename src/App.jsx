518e42352d735e0eeda39fa6139aa4895a904487
titangamerpro <titangamerpro10@gmail.com>
2025-03-18 18:45:37 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import List from "./components/List";
+
+
+
 function App() {
   return (
     <div>
       <h1>To-Do list</h1>
+      <List/>
     </div>
   );
 }
---
+++ src/data/list.js
@@ -0,0 +1,17 @@
+export const lists = [
+    {
+        id:1,
+        checked:false,
+        body:'Шалом'
+    },
+    {
+        id:2,
+        checked:false,
+        body:'Шалом2'
+    },
+    {
+        id:3,
+        checked:false,
+        body:'Шалом3'
+    }
+]
