ca5226d8f345cd7654135067d758ac12e41b7cbf
titangamerpro <titangamerpro10@gmail.com>
2025-03-18 18:55:08 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import "./src/index.css";
+import App from "./src/App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <Reakt.StrictMode>
     <App />
-  </Reakt.StrictMode>
 );
